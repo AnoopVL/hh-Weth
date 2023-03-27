a781f8af8116b9ffc4d6a00dd2d80072ac9435ac
AnoopVL <avlanjekar4@gmail.com>
2023-03-27 09:47:11 +0530

    lendingPool updates

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,7 +1,17 @@
 const { getWeth } = require("../scripts/getWeth")
+const { getNamedAccounts, ethers } = require("hardhat")
 
 async function main() {
   await getWeth()
+  const { deployer } = await getNamedAccounts()
+}
+//lending pool address: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+async function getLendingPool(account) {
+  const lendingPoolAddressProvider = await ethers.getContractAt(
+    "ILendingPoolAddressesProvider",
+    "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+    deployer
+  )
 }
 
 main()
